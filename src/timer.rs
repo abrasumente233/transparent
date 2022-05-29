3fa11018355cd5350c11d77540fe58eb60b3a6fb
abrasumente <345396594@qq.com>
2022-05-29 23:16:58 +0800

    (timer) seems QEMU has a 1.25MHz clock

--- src/timer.rs
+++ src/timer.rs
@@ -1,6 +1,10 @@
 use riscv::register::time;
 use sbi::legacy::set_timer;
 
+//const QEMU_FREQ: usize = 1_000_000;
+const QEMU_FREQ: usize = 12_500_000;
+const TICKS_PER_SEC: usize = 10;
+
 pub(crate) fn init() {
     unsafe {
         riscv::register::sie::set_stimer();
@@ -9,5 +13,5 @@ pub(crate) fn init() {
 }
 
 pub(crate) fn set_next_timer() {
-    set_timer(time::read64() + 10000);
+    set_timer(time::read64() + (QEMU_FREQ / TICKS_PER_SEC) as u64);
 }
