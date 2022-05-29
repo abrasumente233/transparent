9ebc95fb2744fbc2f313a616fb1d63c19d8c00bb
abrasumente <345396594@qq.com>
2022-05-29 14:43:21 +0800

    (intr) keep my lovely multi-asm-naked-fn, although rustc doesn't like it

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-
 #![no_std]
 #![no_main]
+#![feature(naked_functions, core_intrinsics, asm_const)]
 
 mod assembly;
 mod console;
 mod panic;
+mod trap;
 
 #[no_mangle]
 pub fn main() -> ! {
     println!("Hello, world!");
-    panic!("test panic!");
     loop {} 
 }
